c74e24a376b9046ca4c1337f5f4ae68983440f49
Shesh Gaur <sheshg3@gmail.com>
2022-03-22 20:08:52 -0400

    Adjusted NoteSystem to Work With AudioSystem

--- projects/Resonance v2/src/Gameplay/Components/NoteSystem.cpp
+++ projects/Resonance v2/src/Gameplay/Components/NoteSystem.cpp
@@ -52,80 +52,65 @@ NoteSystem::Sptr NoteSystem::FromJson(const nlohmann::json & blob) {
 
 void NoteSystem::Update(float deltaTime) {
 
-	glm::vec3 ppos = _player->GetPosition();
 	glm::vec3 opos = GetGameObject()->GetPosition();
+	Application& app = Application::Get();
 
-	glm::vec3 tpos = ppos - opos;
-	_distance = sqrt(pow(tpos.x, 2) + pow(tpos.y, 2) + pow(tpos.z, 2));
-
+	if (app.isInteracting && isOpen)
+	{
+		//Moved pausing here so the pause happens a frame after interaction
+		//This gives the AudioSystem a frame to play the Pick-Up SFX
+		app.isGamePaused = true;
+	}
 
 	//Animated Objects (raycast based)
-	if (_player->Get<SimpleCameraControl>()->interactionObjectPos == opos && !_player->Get<SimpleCameraControl>()->promptShown) {
+	if (_player->Get<SimpleCameraControl>()->interactionObjectPos == opos) {
 
 		//show interact message
 		_player->Get<SimpleCameraControl>()->ShowPickup();
-	}
 
-	if (glfwGetKey(_window, GLFW_KEY_E)) {
-		if (!isKeyPressed)
+		if (glfwGetKey(_window, GLFW_KEY_E)) {
+			if (!isKeyPressed)
+			{
+				std::cout << "Player interacted with note\n\n";
+				interact();
+				isKeyPressed = true;
+			}
+		}
+		else
 		{
-			std::cout << "Player interacted with note\n\n";
-			interact();
-			isKeyPressed = true;
+			isKeyPressed = false;
 		}
 	}
-	else
-	{
-		isKeyPressed = false;
-	}
 
-	Application& app = Application::Get();
-
-	if (!app.isGamePaused && isOpen)
+	if (!app.isInteracting && !app.isGamePaused && isOpen)
 		interact();
-
 }
 
 void NoteSystem::interact() {
 
-	_distance = (_player->GetPosition().length() - GetGameObject()->GetPosition().length());
-
-	glm::vec3 ppos = _player->GetPosition();
-	glm::vec3 opos = GetGameObject()->GetPosition();
-
+	int windx, windy;
+	glfwGetWindowSize(_window, &windx, &windy);
 	Application& app = Application::Get();
 
-	glm::vec3 tpos = ppos - opos;
-	_distance = sqrt(pow(tpos.x, 2) + pow(tpos.y, 2) + pow(tpos.z, 2));
-
-	//Animated Object (based on raycast)
-	if (_distance <= _interactDistance && _player->Get<SimpleCameraControl>()->interactionObjectPos == opos) { //_player->Get<SimpleCameraControl>()->interactionObjectPos == opos
-		int windx, windy;
-		glfwGetWindowSize(_window, &windx, &windy);
-
-		if (isOpen)
-		{
-			isOpen = false;
-			GetGameObject()->GetScene()->audioManager->Get<AudioManager>()->PlaySoundByName("NotePutdown", 1.0f);
-			GetGameObject()->GetScene()->FindObjectByName(noteName)->Get<GuiPanel>()->IsEnabled = false;
-			std::cout << "Note disabled\n\n";
-			//if (GetGameObject()->GetScene()->FindObjectByName("Main Camera"))
-				//GetGameObject()->GetScene()->FindObjectByName("Main Camera")->Get<SimpleCameraControl>()->IsEnabled = true;
-			//app.isGamePaused = false;
-		}
-		else
-		{
-			isOpen = true;
-
-			GetGameObject()->GetScene()->audioManager->Get<AudioManager>()->PlaySoundByName("NotePickup", 1.0f);
-			GetGameObject()->GetScene()->FindObjectByName(noteName)->Get<GuiPanel>()->IsEnabled = true;
-			GetGameObject()->GetScene()->FindObjectByName(noteName)->Get<RectTransform>()->SetPosition(glm::vec2(windx / 2, windy / 2));
-			GetGameObject()->GetScene()->FindObjectByName(noteName)->Get<RectTransform>()->SetSize(glm::vec2((windx / 4), (windy / 4.5)));
-			std::cout << "Note enabled\n\n";
-			app.isGamePaused = true;
-			app.isInteracting = true;
-		}
+	if (isOpen)
+	{
+		isOpen = false;
+		GetGameObject()->GetScene()->audioManager->Get<AudioManager>()->PlaySoundByName("NotePutdown", 1.0f);
+		GetGameObject()->GetScene()->FindObjectByName(noteName)->Get<GuiPanel>()->IsEnabled = false;
+		std::cout << "Note disabled\n\n";
+		//if (GetGameObject()->GetScene()->FindObjectByName("Main Camera"))
+			//GetGameObject()->GetScene()->FindObjectByName("Main Camera")->Get<SimpleCameraControl>()->IsEnabled = true;
+		//app.isGamePaused = false;
 	}
-
+	else
+	{
+		isOpen = true;
+		GetGameObject()->GetScene()->audioManager->Get<AudioManager>()->PlaySoundByName("NotePickup", 1.0f);
+		GetGameObject()->GetScene()->FindObjectByName(noteName)->Get<GuiPanel>()->IsEnabled = true;
+		GetGameObject()->GetScene()->FindObjectByName(noteName)->Get<RectTransform>()->SetPosition(glm::vec2(windx / 2, windy / 2));
+		GetGameObject()->GetScene()->FindObjectByName(noteName)->Get<RectTransform>()->SetSize(glm::vec2((windx / 4), (windy / 4.5)));
+		std::cout << "Note enabled\n\n";
+		app.isInteracting = true;
+	}
 }
 
